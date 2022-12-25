e4b1dd3d0e5fe9f2191e08e5f7283a96a683fcb3
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2022-12-25 15:45:49 +0500

    first

---
+++ src/redux/store/store.js
@@ -0,0 +1,28 @@
+import { configureStore, combineReducers  } from '@reduxjs/toolkit'
+// import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from '../reducers'
+import rootSaga from '../sagas/rootSaga'
+
+const sagaMiddleware = createSagaMiddleware()
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const rootedReducer = combineReducers(rootReducer) 
+const authPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'profile', 'basket', 'checkout'],
+}
+// const persistedReducer = persistReducer(authPersistConfig, rootReducer)
+
+export default () => {
+  const store = configureStore({
+    reducer: rootedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(sagaMiddleware),
+  })
+  // const persistor = persistStore(store)
+  sagaMiddleware.run(rootSaga)
+  // return { store, persistor }
+  return { store }
+}
