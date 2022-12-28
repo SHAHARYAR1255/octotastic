f11c36a50bb29150f2896577ffee6c9358b3074b
SHAHARYAR1255 <shaharyar.malik2000@gmail.com>
2022-12-28 22:42:04 +0500

    footer added

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,6 +2,7 @@ import * as Route from '@/constants/routes';
 import logo from '@/images/logo-full.png';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import {FacebookFilled, InstagramOutlined,TwitterOutlined } from '@ant-design/icons';
 
 const Footer = () => {
   const { pathname } = useLocation();
@@ -11,31 +12,27 @@ const Footer = () => {
     Route.SHOP
   ];
 
-  return !visibleOnlyPath.includes(pathname) ? null : (
-    <footer className="footer">
-      <div className="footer-col-1">
-        <strong>
-          <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
-          </span>
-        </strong>
+  // return !visibleOnlyPath.includes(pathname) ? null : (
+    return (
+    <footer>
+      <a href="#" className=''><h2>OCTOTASTIC
+      </h2></a>
+
+      <ul className='permalinks'>
+        <li><a href={Route.HOME}>Home</a></li>
+        <li><a href={Route.SHOP}>Shop</a></li>
+        <li><a href={Route.FEATURED_PRODUCTS}>Featured</a></li>
+        <li><a href={Route.RECOMMENDED_PRODUCTS}>Recommended</a></li>
+        <li><a href={Route.CONTACT}>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="#"><FacebookFilled /></a>
+        <a href="#"><InstagramOutlined /></a>
+        <a href="#"><TwitterOutlined /></a>
       </div>
-      <div className="footer-col-2">
-        <img alt="Footer logo" className="footer-logo" src={logo} />
-        <h5>
-          &copy;&nbsp;
-          {new Date().getFullYear()}
-        </h5>
-      </div>
-      <div className="footer-col-3">
-        <strong>
-          <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
-          </span>
-        </strong>
+      <div className="footer__copyright">
+        <small>&copy; Copyright © 2018 -2022 Octotastic, Inc. All rights reserved.</small>
       </div>
     </footer>
   );
